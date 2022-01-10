f1a8fabe9fc74eab200898202c46796c52774b60
Soustin <70994322+Soustin@users.noreply.github.com>
2022-01-10 22:55:28 +0530

    Add files via upload

---
+++ reading_time.py
@@ -0,0 +1,32 @@
+import plotly.figure_factory as pff
+import plotly.graph_objects as go
+import pandas as pd
+import statistics
+import random
+import csv
+
+df = pd.read_csv("newdata.csv")
+data = df["reading_time"].to_list()
+
+def mean_our(counter) :
+    dataset = []
+    for i in range(1, counter):
+        random_index = random.randint(0, len(data))
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+def plotmean(meanlist) :
+    df = meanlist
+    Fig = pff.create_distplot([df], ["Average"], show_hist = False)
+    Fig.show()
+
+def setup() :
+    mean_list = []
+    for i in range(0, 1000):
+        set_of_means = mean_our(30)
+        mean_list.append(set_of_means)
+        plotmean(mean_list)
+
+setup()
